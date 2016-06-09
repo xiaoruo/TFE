fd495863e1e2aec7a9dc9a4b392080d3e6400589
xiaoRuo <1244620324@qq.com>
2016-06-09 20:12:25 +0800

    fix:完善

--- mod/p/demos/index.js
+++ mod/p/demos/index.js
@@ -4,12 +4,20 @@ var Demo = function() {
 Demo.prototype = {
   init: function() {
     console.log('welcome to use TFE');
-    this.setColor();
+    this.showShare();
   },
-  setColor: function() {
-   /* new ModuleDemo({
-      color: 'green'
-    });*/
+  $: function(id) {
+    return document.getElementById(id);
+  },
+  showShare: function() {
+    var _this = this;
+    this.$('J_Share').onclick = function() {
+      _this.$('J_ShareAlert').style.top = '10px';
+    }
+    this.$('J_Copy').onclick = function() {
+      _this.$('J_ShareAlert').style.top = '-130px';
+      alert('复制成功~');
+    }
   }
 };
 
